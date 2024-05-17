4e58c041e046d562ec1214642a975540be617d43
WIN-H0ENK34ANUT\User <ya-www-rom2013@ya.ru>
2024-05-17 16:29:35 +0700

    Третий опыт рабочий

---
+++ VR_Lab2/Assets/trifgger_on_fire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trifgger_on_fire : MonoBehaviour
+{
+    ParticleSystem fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            fire.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            fire.Stop();
+        }
+    }
+}
