5ebcce427b9da8e2dcf401cc611070061168035f
CreamSandvich <chestnyhns@mer.ci.nsu.ru>
2024-06-14 13:37:03 +0700

    Исправил Телепортацию предметов за игроком, которые он брял в руки

---
+++ VR_Lab2/Assets/Scripts/DestroyOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        UnityEngine.Object.Destroy(this);
+    }
+
+    //-------------------------------------------------
+
+
+}
